24ae0b3c91d5d05030fd2868122ea68dd8f60a71
yusuke-818 <pokko.pen@icloud.com>
2020-11-15 01:24:17 +0900

    room_usersテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
   validates :name, presence: true
-  
+
+  has_many :rooms_users
+  has_many :rooms, through: :room_users
 end
